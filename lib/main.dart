647d287dfe2eaf21b7f314c00d389232a19778ca
Sathish <ravichandran.sathish@gmail.com>
2019-10-10 17:41:40 -0400

    Result screen update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_app/answer.dart';
+import 'package:first_app/result.dart';
 import 'package:flutter/material.dart';
-import 'question.dart';
+import './quiz.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,44 +15,69 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
-  //reruns the build when changed
-  void answerChoice() {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your fav color?',
+      'Answertext': [
+        {'text': 'Black', 'score': 1},
+        {'text': 'Blue', 'score': 2},
+        {'text': 'Red', 'score': 3},
+        {'text': 'Green', 'score': 4},
+      ],
+    },
+    {
+      'questionText': 'What\'s your fav place?',
+      'Answertext': [
+        {'text': 'Toronto', 'score': 1},
+        {'text': 'Scarborough', 'score': 2},
+        {'text': 'York', 'score': 3},
+        {'text': 'Markham', 'score': 4},
+      ],
+    },
+    {
+      'questionText': 'What\'s your fav number?',
+      'Answertext': [
+        {'text': '1', 'score': 1},
+        {'text': '5', 'score': 2},
+        {'text': '7', 'score': 3},
+        {'text': '10', 'score': 4},
+      ],
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  //reruns the build when changed
+  void _answerChoice(int score) {
+    _totalScore = _totalScore + score;
+    if (_questionIndex < _questions.length) {}
+    setState(() {
+      _questionIndex = _questionIndex + 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your fav color?',
-        'Answertext': ['Black', 'Blue', 'Red', 'Green'],
-      },
-      {
-        'questionText': 'What\'s your fav place?',
-        'Answertext': ['Toronto', 'Scarborough', 'York', 'Markham'],
-      },
-      {
-        'questionText': 'What\'s your fav number?',
-        'Answertext': ['1', '10', '7'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Dart'),
         ),
-        body: Column(
-          children: [
-            Question(questions[questionIndex]['questionText']),
-            ...(questions[questionIndex]['Answertext'] as List<String>)
-                .map((answer) {
-              return Answer(answerChoice, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                answerQuestions: _answerChoice,
+                questionIndex: _questionIndex,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
---
+++ lib/result.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetQuiz;
+
+  Result(this.resultScore, this.resetQuiz);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 6) {
+      resultText = "you are awesome";
+    } else if (resultScore <= 8) {
+      resultText = "you are okay";
+    } else {
+      resultText = "you are bad";
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          ),
+          FlatButton(
+            textColor: Colors.blue,
+            child: Text('Restart Quiz'),
+            onPressed: resetQuiz,q
+          ),
+        ],
+      ),
+    );
+  }
+}
